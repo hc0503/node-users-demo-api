3528dc10d8f1a8c1b91cb0543d955a4a641e2069
Devdreamsolution <q3construction1@gmail.com>
2021-04-22 15:41:31 +0800

    Change table name to users

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -5,14 +5,14 @@ import { User } from '../models/User';
 
 export async function getUsers(req: Request, res: Response): Promise<Response> {
     const conn = await connect();
-    const courses = await conn.query('SELECT * FROM demos');
+    const courses = await conn.query('SELECT * FROM users');
     return res.json(courses[0]);
 }
 
 export async function createUser(req: Request, res: Response) {
     const newUser: User = req.body;
     const conn = await connect();
-    await conn.query('INSERT INTO demos SET ?', [newUser]);
+    await conn.query('INSERT INTO users SET ?', [newUser]);
     return res.json({
         message: 'User Created'
     });
@@ -21,7 +21,7 @@ export async function createUser(req: Request, res: Response) {
 export async function getUser(req: Request, res: Response): Promise<Response> {
     const id = req.params.id;
     const conn = await connect();
-    const user = await conn.query('SELECT * FROM demos WHERE id = ?', [id]);
+    const user = await conn.query('SELECT * FROM users WHERE id = ?', [id]);
 
     return res.json(user[0]);
 }
@@ -30,7 +30,7 @@ export async function updateUser(req: Request, res: Response) {
     const id = req.params.id;
     const updateUser: User = req.body;
     const conn = await connect();
-    await conn.query('UPDATE demos SET ? WHERE id = ?', [updateUser, id]);
+    await conn.query('UPDATE users SET ? WHERE id = ?', [updateUser, id]);
     return res.json({
         message: 'User updated'
     });
