e68cc6df3766d9e890ea6c3e2cb052729f6c8c3b
Devdreamsolution <q3construction1@gmail.com>
2021-04-22 15:37:22 +0800

    Make controllers

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from 'express';
+
+import { connect } from '../database'
+import { User } from '../models/User';
+
+export async function getUsers(req: Request, res: Response): Promise<Response> {
+    const conn = await connect();
+    const courses = await conn.query('SELECT * FROM demos');
+    return res.json(courses[0]);
+}
+
+export async function createUser(req: Request, res: Response) {
+    const newUser: User = req.body;
+    const conn = await connect();
+    await conn.query('INSERT INTO demos SET ?', [newUser]);
+    return res.json({
+        message: 'User Created'
+    });
+}
+
+export async function getUser(req: Request, res: Response): Promise<Response> {
+    const id = req.params.id;
+    const conn = await connect();
+    const user = await conn.query('SELECT * FROM demos WHERE id = ?', [id]);
+
+    return res.json(user[0]);
+}
+
+export async function updateUser(req: Request, res: Response) {
+    const id = req.params.id;
+    const updateUser: User = req.body;
+    const conn = await connect();
+    await conn.query('UPDATE demos SET ? WHERE id = ?', [updateUser, id]);
+    return res.json({
+        message: 'User updated'
+    });
+}
